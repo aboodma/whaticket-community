226048ce08c9f71ba09efa542f8854301b12003b
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2025-05-31 17:51:01 +0300

    .

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -232,7 +232,7 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            WhaTicket
+            Mira | Whatsapp System
           </Typography>
 
           <div className={classes.themeSwitchContainer}>
